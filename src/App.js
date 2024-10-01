bbb1089f8c633f3e73c840a9afa9ddb75c487c7a
Shabbeer Petersen <Shabbeer.Petersen@capaciti.org.za>
2024-10-01 13:16:50 +0200

    created simple fetch function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Counter, Component } from './script';
 
@@ -6,20 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Counter/>
+        <h1>React App</h1>
       </header>
+        <Counter/>
+        <Component/>
     </div>
   );
 }
--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import React, {useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export function Counter() {
     const [count,setCount] = useState(0);
@@ -8,4 +8,16 @@ export function Counter() {
             <button onClick={()=> setCount(count + 1)}>Click Me</button>
         </div>
     );
+}
+
+export function Component(){
+    const [data, setData] = useState(null);
+
+    useEffect(()=> {
+        fetch('https://api.example.com/data')
+            .then(response=> response.json())
+            .then(data => setData(data));
+    }, []); // empty array means this effect runs once
+
+    return <div>{data ? data.message : 'Loading Data...'}</div>;
 }
